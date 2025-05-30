b74a076020ef0d75fd428fe7f5086630c704a031
k30v1 <k30v1@proton.me>
2025-05-30 08:23:10 +0000

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ get_audience = lambda: json.load(urlopen("https://upload.pypi.org/_/oidc/audienc
 
 if "GITHUB_ACTIONS" in os.environ:
     bearer = "Bearer " + os.environ["ACTIONS_ID_TOKEN_REQUEST_TOKEN"] # workflow must have permissions "id-token: write"
-    url = os.environ["ACTIONS_ID_TOKEN_REQUEST_URL"] + "?" + urlencode({"audience":get_audience()})
+    url = os.environ["ACTIONS_ID_TOKEN_REQUEST_URL"] + f"?audience={quote(get_audience())}"
     #req = Request(url)
     #req.add_header("Authorization", bearer)
     #r = urlopen(req)
