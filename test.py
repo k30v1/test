e03b98c4907ae5b20d6d3c2691ce7dd52d2ba813
k30v1 <k30v1@proton.me>
2025-05-30 07:20:52 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import os
-from urllib.request import Request
+from urllib.request import urlopen, Request
+import json
 
-password = None
+get_audience = lambda: json.load(urlopen("https://upload.pypi.org/_/oidc/audience"))["audience"]
 
-if password is None and "GITHUB_ACTIONS" in os.environ:
-  req = Request(os.environ["ACTIONS_ID_TOKEN_REQUEST_URL"]) # workflow must have permissions "id-token: write"
-  req.add_header("Authorization", f'Bearer {os.environ["ACTIONS_ID_TOKEN_REQUEST_TOKEN"]}')
-  print(req)
+if "GITHUB_ACTIONS" in os.environ: # do "Trusted publishing"
+    url = os.environ["ACTIONS_ID_TOKEN_REQUEST_URL"] # workflow must have permissions "id-token: write"
+    bearer = "Bearer " + os.environ["ACTIONS_ID_TOKEN_REQUEST_TOKEN"]
+    req = Request(f"{url}?audience={get_audience()}")
+    req.add_header("Authorization", bearer)
+    oidc_token = json.load(urlopen(req))["value"]
+    print('o',len(oidc_token)
