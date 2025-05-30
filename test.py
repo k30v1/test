2a2f216d5884a44cb00f56b3a8a45bf8e04c1365
k30v1 <k30v1@proton.me>
2025-05-30 06:44:11 +0000

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ from urllib.request import Request
 password = None
 
 if password is None and "GITHUB_ACTIONS" in os.environ:
-  req = Request(os.environ["ACTIONS_ID_TOKEN_REQUEST_URL"])
+  req = Request(os.environ["ACTIONS_ID_TOKEN_REQUEST_URL"]) # workflow must have "id-token: write"
   req.add_header("Authorization", f'Bearer {os.environ["ACTIONS_ID_TOKEN_REQUEST_TOKEN"]}')
   print(req)
