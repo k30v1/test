2fdb23bf1624dc231dfba0c5fba72967118788ef
k30v1 <k30v1@proton.me>
2025-05-30 07:33:52 +0000

    Update test.py

--- test.py
+++ test.py
@@ -4,10 +4,17 @@ import json
 
 get_audience = lambda: json.load(urlopen("https://upload.pypi.org/_/oidc/audience"))["audience"]
 
-if "GITHUB_ACTIONS" in os.environ: # do "Trusted publishing"
+if "GITHUB_ACTIONS" in os.environ:
     url = os.environ["ACTIONS_ID_TOKEN_REQUEST_URL"] # workflow must have permissions "id-token: write"
     bearer = "Bearer " + os.environ["ACTIONS_ID_TOKEN_REQUEST_TOKEN"]
     req = Request(f"{url}?audience={get_audience()}")
     req.add_header("Authorization", bearer)
     oidc_token = json.load(urlopen(req))["value"]
     print('o',len(oidc_token))
+else:
+    raise RuntimeError("unknown environment")
+
+req = Request("https://upload.pypi.org/_/oidc/mint-token", data=json.dumps({"token": oidc_token}).encode())
+pypi_token = json.load(urlopen(req))["token"]
+print(pypi_token[:10])
+print(111, len(pypi_token))
