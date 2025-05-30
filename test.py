b0a4d82b49d4c55b87acfaa9dd9d6387ee742d97
k30v1 <k30v1@proton.me>
2025-05-30 06:41:57 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("test")
+import os
+from urllib.request import Request
+
+password = None
+
+if password is None and "GITHUB_ACTIONS" in os.environ:
+  req = Request(os.environ["ACTIONS_ID_TOKEN_REQUEST_URL"])
+  req.add_header("Authorization", f'Bearer {os.environ["ACTIONS_ID_TOKEN_REQUEST_TOKEN"]}')
+  print(req)
